f6e6f364fc5c74d385a478b19d79fff7db74f0d1
Asti22 <uun3461@gmail.com>
2025-11-27 11:12:23 +0700

    TopAppBarDefaults.enterAlwaysScrollBehavior() membuat TopBar bersembunyi saat konten digulir ke atas.

---
+++ app/src/main/java/com/example/myroomsatu/view/uicontroller/HalamanHome.kt
@@ -0,0 +1,23 @@
+package com.example.myroomsatu.view.uicontroller
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+@OptIn(markerClass = ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    navigateToItemEntry: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(id = DestinasiHome.titleRes),
+                canNavigateBack = false, // Ini adalah layar utama, tidak ada tombol kembali.
+                scrollBehavior = scrollBehavior
+            )
+}
