aefcede8fbd2b76ebccb8e500b37d4056435d22d
Asti22 <uun3461@gmail.com>
2025-11-27 11:49:40 +0700

    Mendefinisikan rute dan judul untuk Layar Utama yang menampilkan daftar siswa.

---
+++ app/src/main/java/com/example/myroomsatu/view/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.myroomsatu.view.route
+
+import com.example.myroomsatu.R // Import ini diasumsikan dari gambar untuk R.string
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.DataSiswa // Asumsi R.string.DataSiswa ada
+}
