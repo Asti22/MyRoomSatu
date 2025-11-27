4dfdb4afb0dd59d1dc41c55f532d8b2bc11de9a9
Asti22 <uun3461@gmail.com>
2025-11-27 11:49:55 +0700

    Mendefinisikan rute dan judul untuk Layar Input Data siswa baru.

---
+++ app/src/main/java/com/example/myroomsatu/view/route/DestinasiEntry.kt
@@ -0,0 +1,8 @@
+package com.example.myroomsatu.view.route
+
+import com.example.myroomsatu.R // Import ini diasumsikan dari gambar untuk R.string
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.TambahSiswa // Asumsi R.string.TambahSiswa ada
+}
